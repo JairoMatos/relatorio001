b2ec1cdcde350a87f54fa8ee59c385162f90314e
JairoMatos <jairo.oliveira22@gmail.com>
2023-04-16 09:34:15 -0300

    correcao

---
+++ script.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal({ reset: true});
+sr.reveal('h2', { 
+    duration: 1500,
+    rotate: {x:0, y:80, z:0}
+});
+
+sr.reveal('i', {
+    rotate: {x:0, y:80, z:0},
+    duration: 1000,
+
+});
+
+let check = document.querySelector('.selector');
